924c52b2f7c02908f6cea5b74ddeddc8e329c463
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-16 18:33:11 -0500

    s

--- submit.js
+++ submit.js
@@ -9,25 +9,3 @@ function checkEmpty(form){
         alert("Thats wrong fam");
     }
 }
-
-// To
-document.getElementById("fromSpotify").addEventListener("click", function(){
-    checkEmpty();
-});
-document.getElementById("fromApple").addEventListener("click", function(){
-  alert("From apple");
-});
-document.getElementById("fromGoogle").addEventListener("click", function(){
-  alert("From google");
-});
-
-// From
-document.getElementById("toSpotify").addEventListener("click", function(){
-  alert("To spotify");
-});
-document.getElementById("toApple").addEventListener("click", function(){
-  alert("To apple");
-});
-document.getElementById("toGoogle").addEventListener("click", function(){
-  alert("To google");
-});
