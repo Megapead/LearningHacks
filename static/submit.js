00186304e7f8776b9f16861cd9201c9d5873d990
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 00:14:34 -0500

    remove pi chart

--- static/submit.js
+++ static/submit.js
@@ -4,7 +4,7 @@ const form = document.querySelector('form');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    const files = document.querySelector('[type=file]').files;
+    const files = document.querySelector('file').files;
     const formData = new FormData();
 
     for (let i = 0; i < files.length; i++) {
@@ -27,46 +27,46 @@ form.addEventListener('submit', e => {
 
 
 // percentage wheel
-let ctx = document.getElementById('doughnutChart').getContext('2d');
-let labels = ['', 'resemblance'];
-let colorHex = ['#e6e6e6','#43AA8B'];
-let myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [40,60],
-            backgroundColor: colorHex
-        }],
-        labels: labels
-    },
-    options: {
-        responsive: false,
-        hover: {
-            mode: 'index'
-        },
-        legend: {
-            position: ''
-        },
-        plugins: {
-            datalabels: {
-                color: 'white',
-                anchor: 'end',
-                align: 'bottom',
-                offset: 0,
-                borderWidth: 2,
-                borderColor: '#fff',
-                borderRadius: 10,
-                backgroundColor: (context) => {
-                    return context.dataset.backgroundColor;
-                },
-                font: {
-                    weight: 'bold',
-                    size: '10'
-                },
-                formatter: (value) => {
-                    return value + ' %';
-                }
-            }
-        }
-    }
-})
+// let ctx = document.getElementById('doughnutChart').getContext('2d');
+// let labels = ['', 'resemblance'];
+// let colorHex = ['#e6e6e6','#43AA8B'];
+// let myChart = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             data: [40,60],
+//             backgroundColor: colorHex
+//         }],
+//         labels: labels
+//     },
+//     options: {
+//         responsive: false,
+//         hover: {
+//             mode: 'index'
+//         },
+//         legend: {
+//             position: ''
+//         },
+//         plugins: {
+//             datalabels: {
+//                 color: 'white',
+//                 anchor: 'end',
+//                 align: 'bottom',
+//                 offset: 0,
+//                 borderWidth: 2,
+//                 borderColor: '#fff',
+//                 borderRadius: 10,
+//                 backgroundColor: (context) => {
+//                     return context.dataset.backgroundColor;
+//                 },
+//                 font: {
+//                     weight: 'bold',
+//                     size: '10'
+//                 },
+//                 formatter: (value) => {
+//                     return value + ' %';
+//                 }
+//             }
+//         }
+//     }
+// })
