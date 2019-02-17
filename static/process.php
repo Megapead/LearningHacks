d168836499966ff362c885cc913291c6fb587da2
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 01:25:49 -0500

    uploading files

--- static/process.php
+++ static/process.php
@@ -1,35 +1,30 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['files'])) {
-        $errors = [];
-        $path = 'uploads/';
-        $extensions = ['jpg', 'jpeg', 'png', 'gif'];
 
-        $all_files = count($_FILES['files']['tmp_name']);
+if(isset($_POST['submit'])){
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
 
-        for ($i = 0; $i < $all_files; $i++) {
-            $file_name = $_FILES['files']['name'][$i];
-            $file_tmp = $_FILES['files']['tmp_name'][$i];
-            $file_type = $_FILES['files']['type'][$i];
-            $file_size = $_FILES['files']['size'][$i];
-            $file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
+    $fileEx = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileEx));
 
-            $file = $path . $file_name;
-
-            if (!in_array($file_ext, $extensions)) {
-                $errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
+    $allowed = array('jpg','jpeg','png');
+    if(in_array($fileActualExt,$allowed)){
+        if($fileError === 0){
+            if($fileSize < 500000){
+                $fileNamNew = "you.".$fileActualExt;
+                $fileDestination = 'faces/'.$fileNamNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+                echo "Sucess";
+            }else{
+                echo "The file is too big";
             }
-
-            if ($file_size > 2097152) {
-                $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-            }
-
-            if (empty($errors)) {
-                move_uploaded_file($file_tmp, $file);
-            }
+        }else{
+            echo "There was an error with uploading the file";
         }
-
-        if ($errors) print_r($errors);
+    }else{
+        echo "You cannot uplaod that extention";
     }
-}
-?>
+}?>
--- static/submit.js
+++ static/submit.js
@@ -18,25 +18,25 @@ realFileBtn.addEventListener("change", function() {
   }
 });
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const files = document.querySelector('real-file').files;
-    const formData = new FormData();
-
-    for (let i = 0; i < files.length; i++) {
-        let file = files[i];
-
-        formData.append('files[]', file);
-    }
-
-    fetch(url, {
-        method: 'POST',
-        body: formData
-    }).then(response => {
-        console.log(response);
-    });
-});
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//
+//     const files = document.querySelector('real-file').files;
+//     const formData = new FormData();
+//
+//     for (let i = 0; i < files.length; i++) {
+//         let file = files[i];
+//
+//         formData.append('files[]', file);
+//     }
+//
+//     fetch(url, {
+//         method: 'POST',
+//         body: formData
+//     }).then(response => {
+//         console.log(response);
+//     });
+// });
 
 
 
