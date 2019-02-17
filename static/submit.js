fd531e22a49dfc587591678e012879d7d7aceb66
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 03:40:30 -0500

    pi chart

--- static/submit.js
+++ static/submit.js
@@ -1,21 +1,22 @@
-const realFileBtn = document.getElementById("you");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
+// const realFileBtn = document.getElementById("you");
+// const customBtn = document.getElementById("custom-button");
+// const customTxt = document.getElementById("custom-text");
+//
+// customBtn.addEventListener("click", function() {
+//     realFileBtn.click();
+// });
+// realFileBtn.addEventListener("change", function() {
+//     if (realFileBtn.value) {
+//         customTxt.innerHTML = realFileBtn.value.match(
+//             /[\/\\]([\w\d\s\.\-\(\)]+)$/
+//         )[1];
+//     } else {
+//         customTxt.innerHTML = "No file chosen, yet.";
+//     }
+// });
 
-customBtn.addEventListener("click", function() {
-    realFileBtn.click();
-});
-realFileBtn.addEventListener("change", function() {
-    if (realFileBtn.value) {
-        customTxt.innerHTML = realFileBtn.value.match(
-            /[\/\\]([\w\d\s\.\-\(\)]+)$/
-        )[1];
-    } else {
-        customTxt.innerHTML = "No file chosen, yet.";
-    }
-});
-
-
+var percentage = {{result}};
+var percentage2 = 100 - percentage;
 // percentage wheel
 let ctx = document.getElementById('doughnutChart').getContext('2d');
 let labels = ['', 'resemblance'];
@@ -24,7 +25,7 @@ let myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
         datasets: [{
-            data: [{{result}},100 - {{result}}],
+            data: [percentage,percentage2],
             backgroundColor: colorHex
         }],
         labels: labels
