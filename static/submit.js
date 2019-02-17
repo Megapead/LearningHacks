5b4d3bb92b2f9d34080fdea88222ddd6e8c22325
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 02:37:27 -0500

    fixed formating

--- static/submit.js
+++ static/submit.js
@@ -3,16 +3,16 @@ const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
 
 customBtn.addEventListener("click", function() {
-  realFileBtn.click();
+    realFileBtn.click();
 });
 realFileBtn.addEventListener("change", function() {
-  if (realFileBtn.value) {
-    customTxt.innerHTML = realFileBtn.value.match(
-      /[\/\\]([\w\d\s\.\-\(\)]+)$/
-    )[1];
-  } else {
-    customTxt.innerHTML = "No file chosen, yet.";
-  }
+    if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match(
+            /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+    } else {
+        customTxt.innerHTML = "No file chosen, yet.";
+    }
 });
 
 
