25b1540388f28b18d992d9fa62fccba83261bfe6
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-16 20:39:24 -0500

    update

--- submit.js
+++ submit.js
@@ -1,9 +1,7 @@
-<div class="chart" data-percent="73">73%</div>
 
-<script src="/path/to/easy-pie-chart.js"></script>
-<script>
-    var element = document.querySelector('.chart');
-    new EasyPieChart(element, {
-        // your options goes here
-    });
-</script>
+// Chart js And for a doughnut chart
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: options
+});
