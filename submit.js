1d7afcb962e7b214f727ee0f50328a57bb7edfbb
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-16 19:52:24 -0500

    Implementation of pi chart

--- submit.js
+++ submit.js
@@ -1,11 +1,9 @@
+<div class="chart" data-percent="73">73%</div>
 
-
-
-function checkEmpty(form){
-    if(form.name1.value == "a" && form.pass1.value == "b"){
-        alert("ItWorks");
-    }else
-    {
-        alert("Thats wrong fam");
-    }
-}
+<script src="/path/to/easy-pie-chart.js"></script>
+<script>
+    var element = document.querySelector('.chart');
+    new EasyPieChart(element, {
+        // your options goes here
+    });
+</script>
