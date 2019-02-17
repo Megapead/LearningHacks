edce1dadba9c8724c12c233dcc63fabd7493e6ce
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 03:55:44 -0500

    trial

--- static/submit.js
+++ static/submit.js
@@ -52,19 +52,3 @@ let myChart = new Chart(ctx, {
         }
     }
 })
-const realFileBtn = document.getElementById("you");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
-
-customBtn.addEventListener("click", function() {
-    realFileBtn.click();
-});
-realFileBtn.addEventListener("change", function() {
-    if (realFileBtn.value) {
-        customTxt.innerHTML = realFileBtn.value.match(
-            /[\/\\]([\w\d\s\.\-\(\)]+)$/
-        )[1];
-    } else {
-        customTxt.innerHTML = "No file chosen, yet.";
-    }
-});
