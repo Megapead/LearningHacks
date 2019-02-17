da97f91bccffab24801eefa3f60df2171de79970
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-02-17 01:54:56 -0500

    changed to you

--- static/submit.js
+++ static/submit.js
@@ -1,4 +1,4 @@
-const realFileBtn = document.getElementById("real-file");
+const realFileBtn = document.getElementById("you");
 const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
 
